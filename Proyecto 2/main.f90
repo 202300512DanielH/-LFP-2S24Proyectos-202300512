3980d305ed0e046b7095cba04cc78bb7216b61ed
Daniel Hernandez <3031294090108@ingenieria.usac.edu.gt>
2024-10-14 02:50:00 -0600

    Análisis Léxico - Tengo dudas
    Tengo que preguntar como se hace el léxico

--- Proyecto 2/main.f90
+++ Proyecto 2/main.f90
@@ -1,21 +1,24 @@
 PROGRAM Main
     USE ExtractorModule  ! Usamos el módulo que contiene 'ListaCaracteres'
     USE LexicoOne  ! Usamos el módulo LexicoOne para el análisis léxico
+    USE LexicoTwo  ! Usamos el módulo LexicoTwo para limpiar la lista y eliminar "Sin definir"
     IMPLICIT NONE
 
     INTEGER :: i, numLineas
-    CHARACTER(LEN=200), ALLOCATABLE :: texto(:)  ! Declaración correcta del arreglo dinámico de texto
+    CHARACTER(LEN=200), ALLOCATABLE :: texto(:)  ! Arreglo dinámico de texto
     CHARACTER(LEN=200) :: linea  ! Para leer cada línea de entrada
     TYPE(ListaCaracteres) :: lista  ! Variable para almacenar los caracteres
     TYPE(ListaNoReconocidos) :: noReconocidos  ! Lista para caracteres no reconocidos
 
     ! Leer número de líneas a procesar
+    PRINT *, 'Ingrese el número de líneas de texto:'
     READ(*,*) numLineas
 
     ! Asignamos espacio para las líneas
     ALLOCATE(texto(numLineas))
 
     ! Leer las líneas de texto
+    PRINT *, 'Ingrese el texto línea por línea:'
     DO i = 1, numLineas
         READ(*,'(A)') linea
         texto(i) = linea
@@ -29,6 +32,12 @@ PROGRAM Main
 
     ! Imprimir los resultados del análisis léxico
     CALL imprimir_analisis(lista, noReconocidos)
+
+    ! Limpiar la lista eliminando los caracteres con token "Sin definir"
+    !CALL limpiar_lista(lista)
+
+    ! Imprimir la lista filtrada sin los "Sin definir"
+    CALL imprimir_lista_limpia(lista)
 
     ! Liberamos la memoria
     DEALLOCATE(texto)
