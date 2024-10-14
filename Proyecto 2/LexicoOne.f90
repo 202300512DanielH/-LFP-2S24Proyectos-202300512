7a0f4acca72b7ebb4fa1c4c9bc2dcec695c7849b
Daniel Hernandez <3031294090108@ingenieria.usac.edu.gt>
2024-10-14 01:18:33 -0600

    Comentarios de Bloque Validados

--- Proyecto 2/LexicoOne.f90
+++ Proyecto 2/LexicoOne.f90
@@ -3,7 +3,7 @@ MODULE LexicoOne
     USE CaracterModule
     USE ExtractorModule
     IMPLICIT NONE
-    PUBLIC :: analizar_lexico
+    PUBLIC :: analizar_lexico, imprimir_analisis
 
     TYPE :: ListaNoReconocidos
         TYPE(Caracter), ALLOCATABLE :: caracteres(:)  ! Lista dinámica para caracteres no reconocidos
@@ -11,18 +11,46 @@ MODULE LexicoOne
 
 CONTAINS
 
-    ! Subrutina para analizar los caracteres y asignar tokens
+    ! Subrutina para analizar los caracteres y asignar tokens, ignorando comentarios de bloque
     SUBROUTINE analizar_lexico(lista, noReconocidos)
         TYPE(ListaCaracteres), INTENT(INOUT) :: lista
         TYPE(ListaNoReconocidos), INTENT(OUT) :: noReconocidos
-        INTEGER :: i, total_no_reconocidos
+        INTEGER :: i, j, total_no_reconocidos
         CHARACTER(LEN=1) :: caracter_actual
+        LOGICAL :: en_comentario_bloque
 
         total_no_reconocidos = 0
+        en_comentario_bloque = .FALSE.
 
-        DO i = 1, SIZE(lista%caracteres)
+        ! Realizamos el análisis léxico
+        i = 1
+        DO WHILE (i <= SIZE(lista%caracteres))
             caracter_actual = lista%caracteres(i)%valor
 
+            ! Ignorar caracteres dentro de un comentario de bloque
+            IF (en_comentario_bloque) THEN
+                ! Detectar el final del comentario de bloque "*/"
+                IF (caracter_actual == '*' .AND. i < SIZE(lista%caracteres)) THEN
+                    IF (lista%caracteres(i + 1)%valor == '/') THEN
+                        en_comentario_bloque = .FALSE.  ! Fin del comentario de bloque
+                        i = i + 2  ! Saltar "*/"
+                        CYCLE
+                    END IF
+                END IF
+                i = i + 1
+                CYCLE  ! Seguir ignorando mientras estamos en un comentario de bloque
+            END IF
+
+            ! Detectar el inicio de un comentario de bloque "/*"
+            IF (caracter_actual == '/' .AND. i < SIZE(lista%caracteres)) THEN
+                IF (lista%caracteres(i + 1)%valor == '*') THEN
+                    en_comentario_bloque = .TRUE.
+                    i = i + 2  ! Saltar "/*"
+                    CYCLE
+                END IF
+            END IF
+
+            ! Asignación de tokens normales si no estamos en un comentario
             SELECT CASE (caracter_actual)
                 CASE (' ', CHAR(9), CHAR(10))  ! Espacios, tabulaciones o retornos
                     lista%caracteres(i)%token = 'espacio'
@@ -30,61 +58,62 @@ CONTAINS
                 CASE ('A':'Z', 'a':'z')  ! Todas las letras
                     lista%caracteres(i)%token = 'letra'
 
-                CASE ('<')  ! Simbolo de apertura de bloque
+                CASE ('<')  ! Símbolo de apertura de bloque
                     lista%caracteres(i)%token = 'simbolo apertura bloque'
                 
-                CASE ('>')  ! Simbolo de apertura de bloque
+                CASE ('>')  ! Símbolo de cierre de bloque
                     lista%caracteres(i)%token = 'simbolo cierre bloque'
 
-                CASE ('-')  ! Simbolo de bloque
+                CASE ('-')  ! Símbolo de bloque
                     lista%caracteres(i)%token = 'simbolo bloque'
 
-                CASE ('!')  ! Simbolo de bloque
+                CASE ('!')  ! Símbolo de bloque
                     lista%caracteres(i)%token = 'simbolo bloque'
 
-                CASE ('/')  ! Simbolo de comentario
+                CASE ('/')  ! Símbolo de comentario
                     lista%caracteres(i)%token = 'simbolo comentario'
 
-                CASE ('*')  ! Simbolo de comentario
+                CASE ('*')  ! Símbolo de comentario
                     lista%caracteres(i)%token = 'simbolo comentario'
 
                 CASE ('0':'9')  ! Todos los números
                     lista%caracteres(i)%token = 'digito'
 
-                CASE (';')  ! Simbolo de cierre de línea
+                CASE (';')  ! Símbolo de cierre de línea
                     lista%caracteres(i)%token = 'simbolo cierre linea'
 
-                CASE (',')  ! Simbolo de separación
+                CASE (',')  ! Símbolo de separación
                     lista%caracteres(i)%token = 'simbolo separacion'
 
-                CASE ('.')  ! Simbolo de separación
+                CASE ('.')  ! Símbolo de separación
                     lista%caracteres(i)%token = 'simbolo separacion'
 
                 CASE ('"')  ! Comillas
                     lista%caracteres(i)%token = 'comillas'
                 
-                CASE ('(')  ! abrir partentsis
-                    lista%caracteres(i)%token = 'símbolo apertura'
-                
-                CASE (')')  ! cerrar partentsis
-                    lista%caracteres(i)%token = 'símbolo cierre'
+                CASE ('(')  ! Símbolo apertura paréntesis
+                    lista%caracteres(i)%token = 'simbolo apertura'
+
+                CASE (')')  ! Símbolo cierre paréntesis
+                    lista%caracteres(i)%token = 'simbolo cierre'
 
                 CASE DEFAULT
                     lista%caracteres(i)%token = 'desconocido'
-                    ! Aumentamos el contador de no reconocidos y los añadimos a la lista de no reconocidos
                     total_no_reconocidos = total_no_reconocidos + 1
             END SELECT
+
+            i = i + 1  ! Avanzamos al siguiente carácter
         END DO
 
-        ! Ahora reservamos espacio en la lista de no reconocidos
+        ! Reservamos espacio en la lista de no reconocidos
         ALLOCATE(noReconocidos%caracteres(total_no_reconocidos))
 
-        ! Recorremos la lista para agregar los no reconocidos a la lista correspondiente
+        ! Recorremos la lista para agregar los no reconocidos
         total_no_reconocidos = 0
-        DO i = 1, SIZE(lista%caracteres)
-            IF (lista%caracteres(i)%token == 'desconocido') THEN
+        DO j = 1, SIZE(lista%caracteres)
+            IF (lista%caracteres(j)%token == 'desconocido') THEN
                 total_no_reconocidos = total_no_reconocidos + 1
-                noReconocidos%caracteres(total_no_reconocidos) = lista%caracteres(i)
+                noReconocidos%caracteres(total_no_reconocidos) = lista%caracteres(j)
             END IF
         END DO
 
