cbb95bd8bb23d747fa9659eaf203e6d1bdd06a6a
Daniel Hernandez <3031294090108@ingenieria.usac.edu.gt>
2024-10-16 22:39:58 -0600

    Mejora en el analizador léxico

--- Proyecto 2/LexicoOne.f90
+++ Proyecto 2/LexicoOne.f90
@@ -1,151 +1,193 @@
-! Archivo: LexicoOne.f90
 MODULE LexicoOne
     USE CaracterModule
     USE ExtractorModule
     IMPLICIT NONE
     PUBLIC :: analizar_lexico, imprimir_analisis
 
-    TYPE :: ListaNoReconocidos
+    TYPE :: ListaErrores
         TYPE(Caracter), ALLOCATABLE :: caracteres(:)  ! Lista dinámica para caracteres no reconocidos
-    END TYPE ListaNoReconocidos
+    END TYPE ListaErrores
 
 CONTAINS
 
-    ! Subrutina para analizar los caracteres y capturar comentarios de línea
-    SUBROUTINE analizar_lexico(lista, noReconocidos)
-        TYPE(ListaCaracteres), INTENT(INOUT) :: lista
-        TYPE(ListaNoReconocidos), INTENT(OUT) :: noReconocidos
-        INTEGER :: i, j, total_no_reconocidos, fila_actual
-        CHARACTER(LEN=1) :: caracter_actual
-        LOGICAL :: en_comentario_bloque
-        CHARACTER(LEN=500) :: comentario_linea
+SUBROUTINE analizar_lexico(lista, errores)
+    TYPE(ListaCaracteres), INTENT(INOUT) :: lista
+    TYPE(ListaErrores), INTENT(OUT) :: errores
+    INTEGER :: i, j, total_errores, total_reconocidos
+    CHARACTER(LEN=1) :: caracter_actual
+    LOGICAL :: en_comentario_bloque
 
-        total_no_reconocidos = 0
-        en_comentario_bloque = .FALSE.
-        comentario_linea = ""
+    total_errores = 0
+    total_reconocidos = 0
+    en_comentario_bloque = .FALSE.
 
-        ! Realizamos el análisis léxico
-        i = 1
-        DO WHILE (i <= SIZE(lista%caracteres))
-            caracter_actual = lista%caracteres(i)%valor
-            fila_actual = lista%caracteres(i)%posicionFila  ! Obtenemos el número de fila actual
+    i = 1
+    DO WHILE (i <= SIZE(lista%caracteres))
+        caracter_actual = lista%caracteres(i)%valor
 
-            ! Ignorar caracteres dentro de un comentario de bloque
-            IF (en_comentario_bloque) THEN
-                ! Detectar el final del comentario de bloque "*/"
-                IF (caracter_actual == '*' .AND. i < SIZE(lista%caracteres)) THEN
-                    IF (lista%caracteres(i + 1)%valor == '/') THEN
-                        en_comentario_bloque = .FALSE.  ! Fin del comentario de bloque
-                        i = i + 2  ! Saltar "*/"
-                        CYCLE
-                    END IF
-                END IF
-                i = i + 1
-                CYCLE  ! Seguir ignorando mientras estamos en un comentario de bloque
+        ! Detectar inicio de comentario de bloque "/*"
+        IF (caracter_actual == '/' .AND. i < SIZE(lista%caracteres)) THEN
+            IF (lista%caracteres(i + 1)%valor == '*') THEN
+                en_comentario_bloque = .TRUE.
+                lista%caracteres(i)%token = 'comentario'
+                lista%caracteres(i)%tipo = 'Léxico'
+                lista%caracteres(i)%descripcion = 'Comentario de bloque'
+                total_reconocidos = total_reconocidos + 1
+                lista%caracteres(i)%correlativo = total_reconocidos
+                i = i + 1  ! Avanzar al '*'
             END IF
+        END IF
 
-            ! Detectar el inicio de un comentario de bloque "/*"
-            IF (caracter_actual == '/' .AND. i < SIZE(lista%caracteres)) THEN
-                IF (lista%caracteres(i + 1)%valor == '*') THEN
-                    en_comentario_bloque = .TRUE.
-                    i = i + 2  ! Saltar "/*"
+        ! Procesar caracteres dentro del comentario de bloque
+        IF (en_comentario_bloque) THEN
+            lista%caracteres(i)%token = 'comentario'
+            lista%caracteres(i)%tipo = 'Léxico'
+            lista%caracteres(i)%descripcion = 'Comentario de bloque'
+            total_reconocidos = total_reconocidos + 1
+            lista%caracteres(i)%correlativo = total_reconocidos
+
+            ! Detectar el final del comentario de bloque "*/"
+            IF (caracter_actual == '*' .AND. i < SIZE(lista%caracteres)) THEN
+                IF (lista%caracteres(i + 1)%valor == '/') THEN
+                    lista%caracteres(i + 1)%token = 'comentario'
+                    lista%caracteres(i + 1)%tipo = 'Léxico'
+                    lista%caracteres(i + 1)%descripcion = 'Comentario de bloque'
+                    total_reconocidos = total_reconocidos + 1
+                    lista%caracteres(i + 1)%correlativo = total_reconocidos
+                    en_comentario_bloque = .FALSE.
+                    i = i + 2  ! Saltar "*/"
                     CYCLE
                 END IF
             END IF
 
-            ! Detectar el inicio de un comentario de línea "//"
-            IF (caracter_actual == '/' .AND. i < SIZE(lista%caracteres)) THEN
-                IF (lista%caracteres(i + 1)%valor == '/') THEN
-                    ! Capturamos el comentario completo hasta el final de la línea
-                    comentario_linea = comentario_linea // 'Comentario detectado: '
-                    i = i + 2  ! Saltar el "//"
-                    DO WHILE (i <= SIZE(lista%caracteres))
-                        IF (lista%caracteres(i)%posicionFila /= fila_actual) THEN
-                            EXIT  ! Terminamos el comentario al cambiar de fila
-                        END IF
-                        comentario_linea = comentario_linea // '"' // TRIM(lista%caracteres(i)%valor) // '" '
-                        i = i + 1
-                    END DO
-                    PRINT *, TRIM(comentario_linea)  ! Imprimir el comentario detectado
-                    comentario_linea = ""  ! Limpiar la variable para el siguiente comentario
-                    CYCLE  ! Continuar con la siguiente iteración después del comentario
-                END IF
+            i = i + 1
+            CYCLE
+        END IF
+
+        ! Detectar comentario de línea "//"
+        IF (caracter_actual == '/' .AND. i < SIZE(lista%caracteres)) THEN
+            IF (lista%caracteres(i + 1)%valor == '/') THEN
+                DO
+                    lista%caracteres(i)%token = 'comentario'
+                    lista%caracteres(i)%tipo = 'Léxico'
+                    lista%caracteres(i)%descripcion = 'Comentario de línea'
+                    total_reconocidos = total_reconocidos + 1
+                    lista%caracteres(i)%correlativo = total_reconocidos
+                    i = i + 1
+                    IF (i > SIZE(lista%caracteres) .OR. &
+                        lista%caracteres(i)%posicionFila /= lista%caracteres(i - 1)%posicionFila) EXIT
+                END DO
+                CYCLE
             END IF
+        END IF
 
-            ! Asignación de tokens normales si no estamos en un comentario
-            SELECT CASE (caracter_actual)
-                CASE (' ', CHAR(9), CHAR(10))  ! Espacios, tabulaciones o retornos
-                    lista%caracteres(i)%token = 'espacio'
+        ! Asignación de tokens para otros caracteres
+        SELECT CASE (caracter_actual)
+        CASE (' ', CHAR(9), CHAR(10))
+            lista%caracteres(i)%token = 'espacio'
+            lista%caracteres(i)%descripcion = 'Espacio en blanco'
+        
+        CASE ('A':'Z', 'a':'z')
+            lista%caracteres(i)%token = 'letra'
+            lista%caracteres(i)%descripcion = 'Letra'
+            total_reconocidos = total_reconocidos + 1
+            lista%caracteres(i)%correlativo = total_reconocidos
 
-                CASE ('A':'Z', 'a':'z')  ! Todas las letras
-                    lista%caracteres(i)%token = 'letra'
+        CASE ('<')
+            lista%caracteres(i)%token = 'simboloAperturaBloque'
+            lista%caracteres(i)%descripcion = 'Apertura de bloque'
+            total_reconocidos = total_reconocidos + 1
+            lista%caracteres(i)%correlativo = total_reconocidos
 
-                CASE ('<')  ! Símbolo de apertura de bloque
-                    lista%caracteres(i)%token = 'simbolo apertura bloque'
-                
-                CASE ('>')  ! Símbolo de cierre de bloque
-                    lista%caracteres(i)%token = 'simbolo cierre bloque'
+        CASE ('>')
+            lista%caracteres(i)%token = 'simboloCierreBloque'
+            lista%caracteres(i)%descripcion = 'Cierre de bloque'
+            total_reconocidos = total_reconocidos + 1
+            lista%caracteres(i)%correlativo = total_reconocidos
 
-                CASE ('-')  ! Símbolo de bloque
-                    lista%caracteres(i)%token = 'simbolo bloque'
+        CASE ('-')
+            lista%caracteres(i)%token = 'simboloBloque-'
+            lista%caracteres(i)%descripcion = 'Símbolo de bloque -'
+            total_reconocidos = total_reconocidos + 1
+            lista%caracteres(i)%correlativo = total_reconocidos
+        
+        CASE ('!')
+            lista%caracteres(i)%token = 'simboloBloque!'
+            lista%caracteres(i)%descripcion = 'Símbolo de bloque !'
+            total_reconocidos = total_reconocidos + 1
+            lista%caracteres(i)%correlativo = total_reconocidos
 
-                CASE ('!')  ! Símbolo de bloque
-                    lista%caracteres(i)%token = 'simbolo bloque'
+        CASE ('0':'9')
+            lista%caracteres(i)%token = 'digito'
+            lista%caracteres(i)%descripcion = 'Dígito'
+            total_reconocidos = total_reconocidos + 1
+            lista%caracteres(i)%correlativo = total_reconocidos
 
-                CASE ('/')  ! Símbolo de comentario
-                    lista%caracteres(i)%token = 'simbolo comentario'
+        CASE (';')
+            lista%caracteres(i)%token = 'separadorLinea'
+            lista%caracteres(i)%descripcion = 'Separador de línea'
+            total_reconocidos = total_reconocidos + 1
+            lista%caracteres(i)%correlativo = total_reconocidos
 
-                CASE ('*')  ! Símbolo de comentario
-                    lista%caracteres(i)%token = 'simbolo comentario'
+        CASE (',')
+            lista%caracteres(i)%token = 'SeparadorComa'
+            lista%caracteres(i)%descripcion = 'Separador de coma'
+            total_reconocidos = total_reconocidos + 1
+            lista%caracteres(i)%correlativo = total_reconocidos
+        
+        CASE ('.')
+            lista%caracteres(i)%token = 'SeparadorPunto'
+            lista%caracteres(i)%descripcion = 'Separador de punto'
+            total_reconocidos = total_reconocidos + 1
+            lista%caracteres(i)%correlativo = total_reconocidos
 
-                CASE ('0':'9')  ! Todos los números
-                    lista%caracteres(i)%token = 'digito'
+        CASE ('(')
+            lista%caracteres(i)%token = 'parentesisAbierto'
+            lista%caracteres(i)%descripcion = 'Paréntesis de apertura'
+            total_reconocidos = total_reconocidos + 1
+            lista%caracteres(i)%correlativo = total_reconocidos
+        
+        CASE (')')
+            lista%caracteres(i)%token = 'parentesisCerrado'
+            lista%caracteres(i)%descripcion = 'Paréntesis de cierre'
+            total_reconocidos = total_reconocidos + 1
+            lista%caracteres(i)%correlativo = total_reconocidos
 
-                CASE (';')  ! Símbolo de cierre de línea
-                    lista%caracteres(i)%token = 'simbolo cierre linea'
+        CASE ('"')
+            lista%caracteres(i)%token = 'comillas'
+            lista%caracteres(i)%descripcion = 'Comillas'
+            total_reconocidos = total_reconocidos + 1
+            lista%caracteres(i)%correlativo = total_reconocidos
 
-                CASE (',')  ! Símbolo de separación
-                    lista%caracteres(i)%token = 'simbolo separacion'
+        CASE DEFAULT
+            lista%caracteres(i)%token = 'desconocido'
+            lista%caracteres(i)%descripcion = 'Token no reconocido'
+            total_errores = total_errores + 1
+        END SELECT
 
-                CASE ('.')  ! Símbolo de separación
-                    lista%caracteres(i)%token = 'simbolo separacion'
+        lista%caracteres(i)%tipo = 'Léxico'
+        i = i + 1
+    END DO
 
-                CASE ('"')  ! Comillas
-                    lista%caracteres(i)%token = 'comillas'
-                
-                CASE ('(')  ! Símbolo apertura paréntesis
-                    lista%caracteres(i)%token = 'simbolo apertura'
+    ALLOCATE(errores%caracteres(total_errores))
 
-                CASE (')')  ! Símbolo cierre paréntesis
-                    lista%caracteres(i)%token = 'simbolo cierre'
+    total_errores = 0  
 
-                CASE DEFAULT
-                    lista%caracteres(i)%token = 'desconocido'
-                    total_no_reconocidos = total_no_reconocidos + 1
-            END SELECT
+    DO j = 1, SIZE(lista%caracteres)
+        IF (lista%caracteres(j)%token == 'desconocido') THEN
+            total_errores = total_errores + 1
+            errores%caracteres(total_errores) = lista%caracteres(j)
+            errores%caracteres(total_errores)%correlativo = total_errores
+        END IF
+    END DO
+END SUBROUTINE analizar_lexico
 
-            i = i + 1  ! Avanzamos al siguiente carácter
-        END DO
 
-        ! Reservamos espacio en la lista de no reconocidos
-        ALLOCATE(noReconocidos%caracteres(total_no_reconocidos))
 
-        ! Recorremos la lista para agregar los no reconocidos
-        total_no_reconocidos = 0
-        DO j = 1, SIZE(lista%caracteres)
-            IF (lista%caracteres(j)%token == 'desconocido') THEN
-                lista%caracteres(j)%tipo = 'Léxico'
-                total_no_reconocidos = total_no_reconocidos + 1
-                noReconocidos%caracteres(total_no_reconocidos) = lista%caracteres(j)
-            END IF
-        END DO
 
-    END SUBROUTINE analizar_lexico
-
-    ! Subrutina para imprimir los caracteres y sus tokens reconocidos
-    SUBROUTINE imprimir_analisis(lista, noReconocidos)
+    SUBROUTINE imprimir_analisis(lista, errores)
         TYPE(ListaCaracteres), INTENT(IN) :: lista
-        TYPE(ListaNoReconocidos), INTENT(IN) :: noReconocidos
+        TYPE(ListaErrores), INTENT(IN) :: errores
         INTEGER :: i
 
         PRINT *, '--- Carácteres Reconocidos ---'
@@ -153,19 +195,25 @@ CONTAINS
             IF (lista%caracteres(i)%token /= 'desconocido') THEN
                 PRINT *, 'Carácter: ', lista%caracteres(i)%valor, &
                          ' Token: ', lista%caracteres(i)%token, &
+                         ' Tipo: ', lista%caracteres(i)%tipo, &
+                         ' Descripción: ', lista%caracteres(i)%descripcion, &
                          ' Fila: ', lista%caracteres(i)%posicionFila, &
-                         ' Columna: ', lista%caracteres(i)%posicionColumna
+                         ' Columna: ', lista%caracteres(i)%posicionColumna, &
+                         ' Correlativo: ', lista%caracteres(i)%correlativo
             END IF
         END DO
 
         PRINT *, '--- Carácteres No Reconocidos ---'
-        IF (SIZE(noReconocidos%caracteres) == 0) THEN
+        IF (SIZE(errores%caracteres) == 0) THEN
             PRINT *, 'No hay caracteres no reconocidos.'
         ELSE
-            DO i = 1, SIZE(noReconocidos%caracteres)
-                PRINT *, 'Carácter: ', noReconocidos%caracteres(i)%valor, &
-                         ' Fila: ', noReconocidos%caracteres(i)%posicionFila, &
-                         ' Columna: ', noReconocidos%caracteres(i)%posicionColumna
+            DO i = 1, SIZE(errores%caracteres)
+                PRINT *, 'Carácter: ', errores%caracteres(i)%valor, &
+                         ' Tipo: ', errores%caracteres(i)%tipo, &
+                         ' Descripción: ', errores%caracteres(i)%descripcion, &
+                         ' Fila: ', errores%caracteres(i)%posicionFila, &
+                         ' Columna: ', errores%caracteres(i)%posicionColumna, &
+                         ' Correlativo: ', errores%caracteres(i)%correlativo
             END DO
         END IF
     END SUBROUTINE imprimir_analisis
--- Proyecto 2/main.f90
+++ Proyecto 2/main.f90
@@ -1,50 +1,34 @@
 PROGRAM Main
     USE ExtractorModule  ! Usamos el módulo que contiene 'ListaCaracteres'
     USE LexicoOne  ! Usamos el módulo LexicoOne para el análisis léxico
-    USE LexicoTwo  ! Usamos el módulo LexicoTwo para limpiar la lista y eliminar "Sin definir"
-    USE LexicoThree  ! Usamos el módulo LexicoThree para agregar 'Léxico' a la lista y encontrar palabras
     IMPLICIT NONE
 
     INTEGER :: i, numLineas
     CHARACTER(LEN=200), ALLOCATABLE :: texto(:)  ! Arreglo dinámico de texto
     CHARACTER(LEN=200) :: linea  ! Para leer cada línea de entrada
     TYPE(ListaCaracteres) :: lista  ! Variable para almacenar los caracteres
-    TYPE(ListaNoReconocidos) :: noReconocidos  ! Lista para caracteres no reconocidos
-    TYPE(ListaLexico) :: lista_lexico  ! Lista para caracteres con 'Léxico' agregado
-    TYPE(ListaLexico) :: lista_no_reconocidos_lexico  ! Lista para caracteres no reconocidos con 'Léxico' agregado
-    TYPE(ListaLexico) :: lista_palabras  ! Lista para almacenar las palabras encontradas
+    TYPE(ListaErrores) :: errores  ! Lista para caracteres no reconocidos
 
     ! Leer número de líneas a procesar
-    PRINT *, 'Ingrese el número de líneas de texto:'
     READ(*,*) numLineas
 
     ! Asignamos espacio para las líneas
     ALLOCATE(texto(numLineas))
 
     ! Leer las líneas de texto
-    PRINT *, 'Ingrese el texto línea por línea:'
     DO i = 1, numLineas
-        READ(*,'(A)') linea
+        READ(*, '(A)') linea
         texto(i) = linea
     END DO
 
     ! Procesar el texto y extraer los caracteres
     CALL extraer_caracteres(texto, lista)
 
-    ! Analizar los caracteres y asignar tokens
-    CALL analizar_lexico(lista, noReconocidos)
+    ! Analizar los caracteres y asignar tokens, generando lista de errores
+    CALL analizar_lexico(lista, errores)
 
-    ! Limpiar la lista eliminando los caracteres con token "Sin definir"
-    CALL limpiar_lista(lista)
-
-    ! Agregar 'Léxico' a cada carácter de la lista limpia
-    CALL agregar_lexico(lista, lista_lexico)
-
-    ! Encontrar las palabras en la lista de caracteres
-    CALL encontrar_palabras(lista_lexico, lista_palabras)
-
-    ! Imprimir los caracteres reconocidos, no reconocidos y las palabras encontradas
-    CALL imprimir_lista_con_lexico(lista_lexico, noReconocidos, lista_palabras)
+    ! Imprimir los caracteres reconocidos y los errores
+    CALL imprimir_analisis(lista, errores)
 
     ! Liberamos la memoria
     DEALLOCATE(texto)
